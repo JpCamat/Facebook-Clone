6b5c2432255500c6cf390ce9486c045e6876e3fc
JpCamat <31787359+JpCamat@users.noreply.github.com>
2017-09-30 14:58:21 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,152 @@
+<html>
+	
+	<title>Facebook Clone</title>
+	<head>
+		<link type="text/css" rel="stylesheet" href="style.css" />
+
+
+
+
+	</head>
+	<body>
+		<div class="fb-header-base">
+		</div>
+		<div class="fb-header">
+			<div id="img1" class="fb-header"><img src="facebook.png" /></div>
+			<div id="form1" class="fb-header">Email or Phone<br>
+			<input placeholder="Email" type="mail" name="name" /><br>
+			<input type="checkbox" />keep me logged in</div>
+			
+			<div id="form2" class="fb-header">Password<br>
+			<input placeholder="Password" type="password" name="password" /><br>
+			Forgotten your password?
+			</div>
+			</div>
+		</div>
+		<input type="submit" class="submit1" value="login" />
+		<div class="fb-body">
+			<div id="intro1" class="fb-body">Facebook helps you connect and share with the <br>
+			people in your life.</div>
+			<div id="intro2" class="fb-body">Create an account</div>
+			<div id="img2" class="fb-body"><img src="world.png" /></div>
+			<div id="intro3" class="fb-body">It's free and always will be.</div>
+			<div id="form3" class="fb-body">
+				<input placeholder="First Name" type="text" id="namebox" name="name1" />
+				<input placeholder="Last Name" type="text" id="namebox"  /> <br>
+				<input placeholder="Emai" type="text" id="mailbox" /><br>
+				<input placeholder="Re-enter email" type="text" id="mailbox"  /><br>
+				<input placeholder="Password" type="password" id="mailbox"  /><br>
+
+								<select>
+					<option value="Jan">Jan</option>
+					<option value="Feb">Feb</option>
+					<option value="March">March</option>
+					<option value="April">April</option>
+					<option value="May">May</option>
+					<option value="June">June</option>
+					<option value="July">July</option>
+					<option value="Aug">Aug</option>
+					<option value="Sept">Sept</option>
+					<option value="Oct">Oct</option>
+					<option value="Nov">Nov</option>
+					<option value="Dec">Dec</option>
+
+				</select>
+				
+				<select>
+					<option value="31">31</option>
+					<option value="30">30</option>
+					<option value="29">29</option>
+					<option value="28">28</option>
+					<option value="27">27</option>
+					<option value="26">26</option>
+					<option value="25">25</option>
+					<option value="24">24</option>
+					<option value="23">23</option>
+					<option value="22">22</option>
+					<option value="21">21</option>
+					<option value="20">20</option>
+					<option value="19">19</option>
+					<option value="18">18</option>
+					<option value="17">17</option>
+					<option value="16">16</option>
+					<option value="15">15</option>
+					<option value="14">14</option>
+					<option value="13">13</option>
+					<option value="12">12</option>
+					<option value="11">11</option>
+					<option value="10">10</option>
+					<option value="9">9</option>
+					<option value="8">8</option>
+					<option value="7">7</option>
+					<option value="7">7</option>
+					<option value="6">6</option>
+					<option value="5">5</option>
+					<option value="4">4</option>
+					<option value="3">3</option>
+					<option value="2">2</option>
+					<option value="1">1</option>
+					<option value=""> </option>
+
+				</select>
+				
+				<select>
+					<option value="2017">2017</option>
+					<option value="2016">2016</option>
+					<option value="2015">2015</option>
+					<option value="2014">2014</option>
+					<option value="2013">2013</option>
+					<option value="2012">2012</option>
+					<option value="2011">2011</option>
+					<option value="2010">2010</option>
+					<option value="2009">2009</option>
+					<option value="2008">2008</option>
+					<option value="2007">2007</option>
+					<option value="2006">2006</option>
+					<option value="2005">2005</option>
+					<option value="2004">2004</option>
+					<option value="2003">2003</option>
+					<option value="2002">2002</option>
+					<option value="2001">2001</option>
+					<option value="2000">2000</option>
+					<option value="1999">1999</option>
+					<option value="1998">1998</option>
+					<option value="1997">1997</option>
+					<option value="1996">1996</option>
+					<option value="1995">1995</option>
+					<option value="1994">1994</option>
+					<option value="1993">1993</option>
+					<option value="1992">1992</option>
+					<option value="1991">1991</option>
+					<option value="1990">1990</option>
+					<option value="1989">1989</option>
+					<option value="1988">1988</option>
+					<option value="1987">1987</option>
+					<option value="1986">1986</option>
+					<option value="1985">1985</option>
+					<option value="1984">1984</option>
+					<option value="1983">1983</option>
+					<option value="1982">1982</option>
+					<option value="1981">1981</option>
+				</select>
+				
+				<input type="radio" id="r-b" name="sex" value="male" />Male
+				<input type="radio" id="r-b" name="sex" value="female" />Female<br><br>
+				<p id="intro4">By clicking Create an account, you agree to our Terms and that 
+				you have read our Data Policy, including our Cookie Use.</p>
+				<input type="submit" class="button2" value="Create an account" />
+				<br><hr>
+				<p id="intro5">Create a Page for a celebrity, band or business.</p>
+				
+			</div>
+			
+		</div>
+		<div class="fb-body-footer">
+				<div id="fb-body-footer-base" class="fb-body-footer">English (US) Tagalog Bisaya Español 日本語 한국어 中文(简体) العربي Português (Brasil)Français (France)Deutsch <br><hr>
+				Sign Up	     &nbsp;   Log In 	   &nbsp; 	Messenger	&nbsp; Facebook 	&nbsp;    Lite	&nbsp;Mobile	&nbsp;	Find Friends	&nbsp; People	Pages	&nbsp;	Places &nbsp;	Games &nbsp;	Locations
+				Celebrities	  &nbsp;  Marketplace &nbsp;	Groups		&nbsp; Recipes	&nbsp;	Sports&nbsp;	Moments	&nbsp;	Instagram	&nbsp;	About &nbsp;	Create Ad	&nbsp; Create &nbsp;	Page &nbsp;	Developers
+				Careers		  &nbsp;  Privacy	   &nbsp; 	Cookies		&nbsp; Ad Choices &nbsp; 	Terms	&nbsp; Help</div>
+			</div>
+	</body>
+	
+</html>
